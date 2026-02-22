7c60bc9a0db715054c27ffec8a64f971ef933605
Hasan Khalil <mian.hasan.khalil@gmail.com>
2026-02-22 01:29:40 -0500

    Fix JS syntax issues.

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.29"
+        #define VERSION "0.1.30"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2026-02-22 00:43:37.519294"
+        #define BUILD_TIMESTAMP "2026-02-22 01:20:41.935382"
     #endif
     
