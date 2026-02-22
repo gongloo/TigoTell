88ca21e5421f90e7f6dcc64b55b3faeb564aa6ed
Hasan Khalil <mian.hasan.khalil@gmail.com>
2026-02-22 00:49:07 -0500

    Remove spurious animation, fix minor syntax issue.

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.28"
+        #define VERSION "0.1.29"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2026-02-22 00:38:47.225716"
+        #define BUILD_TIMESTAMP "2026-02-22 00:43:37.519294"
     #endif
     
