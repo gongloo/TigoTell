39873da6d6c785cef83a3a10d84b92ae1cac9555
Hasan Khalil <mian.hasan.khalil@gmail.com>
2026-02-22 02:55:42 -0500

    Improve defaults in sample config files.

--- src/config.example.h
+++ src/config.example.h
@@ -7,13 +7,11 @@
 #define EN_PIN 21
 
 // InfluxDB stats reporting.
-/*
-const IPAddress kInfluxHost(192, 168, 8, 1);  // Influx DB Host
-constexpr uint16_t kInfluxPort = 8096;        // Influx DB UDP Port
+const IPAddress kInfluxHost(192, 168, 8, 1); // Influx DB Host
+constexpr uint16_t kInfluxPort = 8096;       // Influx DB UDP Port
 // InfluxDB stats push interval.
 #define PUSH_STATS_INTERVAL_IN_S 10
-const char* MEASUREMENT_NAME = "tigotell_v0";
-*/
+const char *MEASUREMENT_NAME = "tigotell_v0";
 
 // Minimum time between outputs for a given node.
 #define MIN_SECONDS_BETWEEN_NODE_UPDATES 10
