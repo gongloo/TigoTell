109380168742575e971e462d292a0926f2e75370
Hasan Khalil <mian.hasan.khalil@gmail.com>
2026-02-22 00:29:11 -0500

    Add Total Power stat card.

--- include/Version.h
+++ include/Version.h
@@ -1,9 +1,9 @@
 
     // AUTO GENERATED FILE, DO NOT EDIT
     #ifndef VERSION
-        #define VERSION "0.1.26"
+        #define VERSION "0.1.27"
     #endif
     #ifndef BUILD_TIMESTAMP
-        #define BUILD_TIMESTAMP "2026-02-19 16:52:18.908904"
+        #define BUILD_TIMESTAMP "2026-02-22 00:18:40.540957"
     #endif
     
